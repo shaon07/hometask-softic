ec046549877e5a55420c7d0441ef9f6cc278d89c
shaon07 <alishaon078@gmail.com>
2022-11-22 12:51:11 +0600

    added profile card button in detail page

--- pages/detail/index.tsx
+++ pages/detail/index.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable @next/next/no-img-element */
 import { Card, Col, Divider, Row } from 'antd';
 import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { HiDotsHorizontal } from "react-icons/hi";
 import { useSelector } from 'react-redux';
 import { RootState } from '../../redux/store';
 import { authorDetailType, authorType } from '../../types';
@@ -16,6 +17,7 @@ export default function Index() {
   const comments = useSelector((state: RootState) => state.getUser.comments.filter(item => item.postId === id))
   const [author, setAuthor] = useState<authorType>({} as authorType);
   const [authorDetail, setAuthorDetail] = useState<authorDetailType>({} as authorDetailType);
+  const [showProfile, setShowProfile] = useState<Boolean>(false)
 
   useEffect(() => {
     if (data) {
@@ -49,8 +51,13 @@ export default function Index() {
     }
   }, [data]);
 
+  const handleProfileShow = (e: React.MouseEvent<HTMLHeadingElement>) => {
+    e.stopPropagation()
+    setShowProfile(true)
+  }
+
   return (
-    <Col>
+    <Col onClick={() => setShowProfile(false)}>
       <Row>
         <div className={`${styles.detailWrapper}`}>
           <Col xs={24} md={24} lg={12}>
@@ -61,10 +68,27 @@ export default function Index() {
               <div className={`${styles.authorInfo}`}>
                 <img src={author.url} alt="author" width={30} height={30} />
                 <div className={`${styles.authorBody}`}>
-                  <h5>{authorDetail.name}</h5>
+                  <h5 onClick={handleProfileShow}>{authorDetail.name} <HiDotsHorizontal /></h5>
                   <span>{authorDetail.email}</span>
                 </div>
+
+                {
+                  showProfile && <div className={`${styles.authorProfile}`}>
+                    <Card
+                      className="space-align-block"
+                      cover={<img src={author.url} alt="card" height={150} />}
+                    >
+                      <h4>{authorDetail.name} <span>@{authorDetail.username}</span></h4>
+                      <h5>{authorDetail.email}</h5>
+                      <a>{authorDetail.phone.replace("x56442", '')}</a>
+                      <a>{authorDetail.website}</a>
+                      <a>from : {authorDetail.address.city}</a>
+                    </Card>
+                  </div>
+                }
               </div>
+
+
 
               <div className={`${styles.post}`}>
                 <h2>{data?.title}</h2>
